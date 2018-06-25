89360cb6e35f8ae0721dd460527c0b2f52bbce19
LongShengWen <1142056181@qq.com>
2018-06-25 10:25:26 +0800

    Initial Commit

---
+++ scrollViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  scrollViewDemo
+//
+//  Created by 粟黄 on 2018/6/25.
+//  Copyright © 2018年 sw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ scrollViewDemoTests/scrollViewDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  scrollViewDemoTests.m
+//  scrollViewDemoTests
+//
+//  Created by 粟黄 on 2018/6/25.
+//  Copyright © 2018年 sw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface scrollViewDemoTests : XCTestCase
+
+@end
+
+@implementation scrollViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
